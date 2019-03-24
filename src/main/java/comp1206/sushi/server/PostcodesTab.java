362c7e1d9732064e32f556c292d05ecee6acb5e8
Mihai Nuica <mihainuica@gmail.com>
2019-03-24 23:01:16 +0000

    -Add function implemented on postcodes -Remove function implemented on postcodes

--- src/main/java/comp1206/sushi/server/PostcodesTab.java
+++ src/main/java/comp1206/sushi/server/PostcodesTab.java
@@ -4,6 +4,8 @@ import comp1206.sushi.common.Postcode;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class PostcodesTab extends JPanel
@@ -18,6 +20,7 @@ public class PostcodesTab extends JPanel
     ButtonGroup buttons = new ButtonGroup();
     JButton addButton = new JButton("Add");
     JButton removeButton = new JButton("Remove");
+    JTextField inputField = new JTextField();
 
     private void updatePostCodes()
     {
@@ -42,16 +45,50 @@ public class PostcodesTab extends JPanel
         tablePanel.add(table,BorderLayout.CENTER);
         updatePostCodes();
 
+        addButton.addActionListener(new AddButtonListener());
+        removeButton.addActionListener(new DeleteButtonListener());
         buttons.add(addButton);
         buttons.add(removeButton);
+
+        buttonsPanel.setLayout(new GridLayout(3,1));
+        buttonsPanel.add(inputField);
         buttonsPanel.add(addButton);
         buttonsPanel.add(removeButton);
+
 
 
         this.add(tablePanel);
         this.add(buttonsPanel);
     }
 
+    class AddButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            String input = inputField.getText();
+            server.addPostcode(input);
+            inputField.setText("");
+            updatePostCodes();
+        }
+    }
+
+    class DeleteButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            Postcode selectedValue = listModel.getElementAt(table.getSelectedIndex());
+            try
+            {
+                server.removePostcode(selectedValue);
+            }
+            catch (ServerInterface.UnableToDeleteException exception)
+            {
+                System.out.println("Unable to delete. No such element");
+            }
+
+            updatePostCodes();
+        }
+    }
 
 
 }
