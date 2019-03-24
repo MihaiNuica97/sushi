8ae16db044f484f99401817dce6c38ad7740e7d4
Mihai Nuica <mihainuica@gmail.com>
2019-03-24 23:46:34 +0000

    -WORK IN PROGRESS: Add drone function

--- src/main/java/comp1206/sushi/server/DronesTab.java
+++ src/main/java/comp1206/sushi/server/DronesTab.java
@@ -5,6 +5,8 @@ import comp1206.sushi.common.Drone;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class DronesTab extends JPanel
@@ -18,8 +20,8 @@ public class DronesTab extends JPanel
 
 
     JPanel buttonsPanel = new JPanel();
-    JButton addButton = new JButton("Add Drone");
-
+    JButton addButton = new JButton("Add New Drone");
+    JButton removeBtn = new JButton("Remove Drone");
     JPanel contentPanel = new JPanel();
 
     public DronesTab(ServerInterface server)
@@ -39,8 +41,10 @@ public class DronesTab extends JPanel
         tablePanel.add(scrollPane);
         updateDrones();
 
+        addButton.addActionListener(new AddButtonListener());
         buttonsPanel.setLayout(new FlowLayout());
         buttonsPanel.add(addButton);
+        buttonsPanel.add(removeBtn);
 
 
     }
@@ -57,4 +61,56 @@ public class DronesTab extends JPanel
         }
     }
 
+
+
+    class AddButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            JFrame addDroneFrame = new JFrame("Add New Drone");
+            addDroneFrame.setVisible(true);
+            addDroneFrame.setSize(500,300);
+            addDroneFrame.setLocationRelativeTo(null);
+            addDroneFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            addDroneFrame.add(new JLabel("Please specify drone speed:"));
+            JTextField inputField = new JTextField();
+//            addDroneFrame.add(inputField);
+
+
+
+        }
+    }
+
+//    class DeleteButtonListener implements ActionListener
+//    {
+//        public void actionPerformed(ActionEvent e)
+//        {
+//            Postcode selectedValue = listModel.getElementAt(table.getSelectedIndex());
+//            ArrayList<Supplier> supplierList = new ArrayList<>(server.getSuppliers());
+//            boolean isUsed = false;
+//            for(Supplier supplier: supplierList)
+//            {
+//                if(selectedValue == supplier.getPostcode())
+//                {
+//                    isUsed = true;
+//                    break;
+//                }
+//            }
+//            if(isUsed)
+//            {
+//                System.out.println("Cannot delete. Postcode is being used by a supplier");
+//            }
+//            else
+//            {
+//
+//                try {
+//                    server.removePostcode(selectedValue);
+//                } catch (ServerInterface.UnableToDeleteException exception) {
+//                    System.out.println("Unable to delete. No such element");
+//                }
+//                updatePostCodes();
+//            }
+//        }
+//    }
 }
