370be9cc021bb273b62812b7a927322d8131c882
Mihai Nuica <mihainuica@gmail.com>
2019-03-25 01:15:56 +0000

    -Remove drone function complete

--- src/main/java/comp1206/sushi/server/DronesTab.java
+++ src/main/java/comp1206/sushi/server/DronesTab.java
@@ -42,6 +42,7 @@ public class DronesTab extends JPanel
         updateDrones();
 
         addButton.addActionListener(new AddButtonListener());
+        removeBtn.addActionListener(new DeleteButtonListener());
         buttonsPanel.setLayout(new FlowLayout());
         buttonsPanel.add(addButton);
         buttonsPanel.add(removeBtn);
@@ -56,7 +57,7 @@ public class DronesTab extends JPanel
         tableModel.setRowCount(0);
         for(Drone drone: droneList)
         {
-            Object [] row = {drone.getName(),drone.getSpeed(),server.getDroneStatus(drone)};
+            Object [] row = {drone,drone.getSpeed(),server.getDroneStatus(drone)};
             tableModel.addRow(row);
         }
     }
@@ -97,35 +98,24 @@ public class DronesTab extends JPanel
         }
     }
 
-//    class DeleteButtonListener implements ActionListener
-//    {
-//        public void actionPerformed(ActionEvent e)
-//        {
-//            Postcode selectedValue = listModel.getElementAt(table.getSelectedIndex());
-//            ArrayList<Supplier> supplierList = new ArrayList<>(server.getSuppliers());
-//            boolean isUsed = false;
-//            for(Supplier supplier: supplierList)
-//            {
-//                if(selectedValue == supplier.getPostcode())
-//                {
-//                    isUsed = true;
-//                    break;
-//                }
-//            }
-//            if(isUsed)
-//            {
-//                System.out.println("Cannot delete. Postcode is being used by a supplier");
-//            }
-//            else
-//            {
-//
-//                try {
-//                    server.removePostcode(selectedValue);
-//                } catch (ServerInterface.UnableToDeleteException exception) {
-//                    System.out.println("Unable to delete. No such element");
-//                }
-//                updatePostCodes();
-//            }
-//        }
-//    }
+    class DeleteButtonListener implements ActionListener
+    {
+
+        public void actionPerformed(ActionEvent e)
+        {
+            if (dronesTable.getSelectedRow() != -1)
+            {
+                Drone selectedDrone = (Drone)tableModel.getValueAt(dronesTable.getSelectedRow(), 0);
+                try
+                {
+                    server.removeDrone(selectedDrone);
+                }
+                catch (ServerInterface.UnableToDeleteException ex)
+                {
+                    ex.printStackTrace();
+                }
+                updateDrones();
+            }
+        }
+    }
 }
